928eb557372e030220c2da8bebcbadd5c0eae77b
TRISXTECH <117633938+TRISXTECH@users.noreply.github.com>
2023-03-27 11:32:16 +0530

    Add files via upload

---
+++ DataAccess/DatabaseContext.cs
@@ -0,0 +1,236 @@
+﻿namespace DataAccess
+{
+    using System.Data.Entity;
+    using DomainModel;
+
+    public partial class DatabaseContext : DbContext
+    {
+        public DatabaseContext()
+            : base("name=DatabaseContext")
+        {
+        }
+
+        public virtual DbSet<Account> Accounts { get; set; }
+        public virtual DbSet<Album> Albums { get; set; }
+        public virtual DbSet<AlbumTrackMaster> AlbumTrackMasters { get; set; }
+        public virtual DbSet<BankDetail> BankDetails { get; set; }
+        public virtual DbSet<Coupon> Coupons { get; set; }
+        public virtual DbSet<EpTrackMaster> EpTrackMasters { get; set; }
+        public virtual DbSet<ExtendedPlay> ExtendedPlays { get; set; }
+        public virtual DbSet<PriceInfo> PriceInfoes { get; set; }
+        public virtual DbSet<PurchaseRecord> PurchaseRecords { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<SingleTrackDetail> SingleTrackDetails { get; set; }
+        public virtual DbSet<SoloTrackMaster> SoloTrackMasters { get; set; }
+        public virtual DbSet<UserDetail> UserDetails { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Email)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Account>()
+                .Property(e => e.Password)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.BankDetails)
+                .WithOptional(e => e.Account)
+                .HasForeignKey(e => e.Account_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.Coupons)
+                .WithOptional(e => e.Account)
+                .HasForeignKey(e => e.Created_By)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Account>()
+                .HasMany(e => e.PurchaseRecords)
+                .WithOptional(e => e.Account)
+                .HasForeignKey(e => e.Account_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Account>()
+                .HasOptional(e => e.UserDetail)
+                .WithRequired(e => e.Account)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Album>()
+                .Property(e => e.Album_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Album>()
+                .HasMany(e => e.AlbumTrackMasters)
+                .WithOptional(e => e.Album)
+                .HasForeignKey(e => e.Album_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeFirstName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeLastName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeBankName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeBankIfscNumber)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeBankBranch)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<BankDetail>()
+                .Property(e => e.PayeeBankAccountNumber)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Coupon>()
+                .Property(e => e.Coupon_Code)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Coupon>()
+                .Property(e => e.TobeAppliedOnCategory)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Coupon>()
+                .HasMany(e => e.PurchaseRecords)
+                .WithOptional(e => e.Coupon)
+                .HasForeignKey(e => e.CouponCodeApplied);
+
+            modelBuilder.Entity<ExtendedPlay>()
+                .Property(e => e.Ep_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<ExtendedPlay>()
+                .HasMany(e => e.EpTrackMasters)
+                .WithOptional(e => e.ExtendedPlay)
+                .HasForeignKey(e => e.Ep_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<PriceInfo>()
+                .Property(e => e.Category_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PurchaseRecord>()
+                .Property(e => e.Purchased_Category)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<PurchaseRecord>()
+                .HasMany(e => e.Albums)
+                .WithOptional(e => e.PurchaseRecord)
+                .HasForeignKey(e => e.PurchaseTrack_RefNo)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<PurchaseRecord>()
+                .HasMany(e => e.ExtendedPlays)
+                .WithOptional(e => e.PurchaseRecord)
+                .HasForeignKey(e => e.PurchaseTrack_RefNo)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<PurchaseRecord>()
+                .HasMany(e => e.SoloTrackMasters)
+                .WithOptional(e => e.PurchaseRecord)
+                .HasForeignKey(e => e.PurchaseTrack_RefNo)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<Role>()
+                .Property(e => e.Role_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<Role>()
+                .HasMany(e => e.Accounts)
+                .WithOptional(e => e.Role)
+                .HasForeignKey(e => e.Account_Role)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.TrackTitle)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ArtistName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ArtistSpotifyUrl)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.Genre)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.CopyrightClaimerName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.AuthorName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ComposerName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ArrangerName)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ISRC_Number)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.LyricsLanguage)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.TrackZipFileLink)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .Property(e => e.ArtworkFileLink)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .HasMany(e => e.AlbumTrackMasters)
+                .WithOptional(e => e.SingleTrackDetail)
+                .HasForeignKey(e => e.Track_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .HasMany(e => e.EpTrackMasters)
+                .WithOptional(e => e.SingleTrackDetail)
+                .HasForeignKey(e => e.Track_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<SingleTrackDetail>()
+                .HasMany(e => e.SoloTrackMasters)
+                .WithOptional(e => e.SingleTrackDetail)
+                .HasForeignKey(e => e.Track_Id)
+                .WillCascadeOnDelete();
+
+            modelBuilder.Entity<UserDetail>()
+                .Property(e => e.User_First_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserDetail>()
+                .Property(e => e.User_Last_Name)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserDetail>()
+                .Property(e => e.User_Mobile_Number)
+                .IsUnicode(false);
+
+            modelBuilder.Entity<UserDetail>()
+                .Property(e => e.User_Address)
+                .IsUnicode(false);
+        }
+    }
+}
---
+++ DataAccess/Migrations/Configuration.cs
@@ -0,0 +1,64 @@
+﻿namespace DataAccess.Migrations
+{
+    using DomainModel;
+    using System;
+    using System.Collections.Generic;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DataAccess.DatabaseContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(DataAccess.DatabaseContext context)
+        {
+            //Role initializing
+            var roles = new List<Role>
+            {
+            new Role{Role_Name="superadmin"},
+            new Role{Role_Name="admin"},
+            new Role{Role_Name="user"}
+            };
+
+            roles.ForEach(s => context.Roles.Add(s));
+            context.SaveChanges();
+
+            //First SuperAdmin Account Initializing
+            if (context.Accounts.Any(account => account.Email == "superadmin@site.com"))
+            {
+                Account account = context.Accounts.Where(acc => acc.Email == "superadmin@site.com").FirstOrDefault();
+                account.Account_Role = context.Roles.Where(role => role.Role_Name == "superadmin").FirstOrDefault().Id;
+                context.Entry(account).State = EntityState.Modified;
+                context.SaveChanges();
+            }
+            else
+            {
+                Account account = new Account();
+                var newAccountId = Guid.NewGuid();
+                account.Id = newAccountId;
+                account.Email = "superadmin@site.com";
+                account.Password = "Super@123";
+                account.Account_Creation_Date = DateTime.Now;
+                account.Account_Status = 1;
+                account.Account_Role = context.Roles.Where(role => role.Role_Name == "superadmin").FirstOrDefault().Id;
+
+                UserDetail ud = new UserDetail();
+                ud.User_Account_Id = newAccountId;
+                ud.User_First_Name = "Koushik";
+                ud.User_Last_Name = "Saha";
+                ud.User_Mobile_Number = "+917699978887";
+                ud.User_Address = "N/A";
+
+                context.Accounts.Add(account);
+                context.SaveChanges();
+
+                context.UserDetails.Add(ud);
+                context.SaveChanges();
+            }
+        }
+    }
+}
